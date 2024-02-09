398f4aca1b944826e7d61b2466f32be50ef5597e
DaniellEsc <edisson.escandon.est@tecazuay.edu.ec>
2024-02-09 09:13:31 -0500

    creaion carperta build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import TicTac from './components/TicTac';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen bg-gradient-to-r from-blue-400 via-blue-400 to-blue-500">
+      <h2 className='text-5xl font-bold pt-12'>
+        XOXOXO
+      </h2>
+      <TicTac/>
     </div>
   );
 }
